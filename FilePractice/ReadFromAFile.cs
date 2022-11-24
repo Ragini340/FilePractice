4a4cc9eaaed38fd5a47fedf6a8c722d7ce033769
Ragini340 <ragini31bxr@gmail.com>
2022-11-24 17:16:28 +0530

    Add project files.

---
+++ FilePractice/ReadFromAFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FilePractice
+{
+    class ReadFromAFile
+    {
+        public void DataReading()
+        { // Taking a new input stream that is C://Users/RRagini/source/repos/FilePractice.txt and opening it
+            StreamReader obj = new StreamReader("C://Users/RRagini/source/repos/FilePractice.txt");
+            Console.WriteLine("Content of the file");
+            // This is use to specify from where to start reading input stream
+            obj.BaseStream.Seek(0, SeekOrigin.Begin);
+            // Reading line from input stream
+            string str = obj.ReadLine();
+            // Reading the whole file line by line
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = obj.ReadLine();
+            }
+            Console.ReadLine();
+            // Closing the stream
+            obj.Close();
+        }
+
+        static void Main(string[] arg)
+        {
+            ReadFromAFile obj = new ReadFromAFile();
+            obj.DataReading();
+        }
+    }
+}
---
+++ FilePractice/WriteToAFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FilePractice
+{
+    class WriteToAFile
+    {
+        public void Data()
+        {
+            //Creating a file at the below location
+            StreamWriter obj = new StreamWriter("C://Users/RRagini/source/repos/FilePractice.txt");
+            // Writing on the console screen
+            Console.WriteLine("Enter the message to be written on the file");
+            //Reading input from the user
+            string str = Console.ReadLine();
+            //Writing a line in buffer
+            obj.WriteLine(str);
+            //Writing in output stream
+            obj.Flush();
+            // To close the stream
+            obj.Close();
+        }
+    
+    static void Main(string[] args)
+    {
+        WriteToAFile obj = new WriteToAFile();
+        obj.Data();
+        Console.ReadKey();
+    }
+  }
+}
